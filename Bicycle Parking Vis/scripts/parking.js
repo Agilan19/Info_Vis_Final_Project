a4e3320a7407e428b5dcc1b2085b2bf12745f0c7
Kenyawest <judegantony@gmail.com>
2020-04-10 01:23:33 -0400

    more changes

--- Bicycle Parking Vis/scripts/parking.js
+++ Bicycle Parking Vis/scripts/parking.js
@@ -1,24 +1,24 @@
 
 window.onload = function () {
 
-  var width = 800;
-  var height = 600;
+  const width = 800;
+  const height = 600;
 
-  var svg = d3.select("#vis").append("svg")
+  let svg = d3.select("#vis").append("svg")
       .attr("width", width).attr("height", height)
 
 
-  var url = "data/city_wards_data.geojson";
+  const data = "data/city_wards_data.geojson";
 
-  d3.json(url, function (err, json) {
-    console.log(json);
-
-
-    var projection = d3.geoMercator().fitSize([width, height], json);
-    var path = d3.geoPath().projection(projection);
+  d3.json(data, function (err, geojson) {
+    console.log(geojson);
 
 
-    svg.selectAll("path").data(json.features).enter().append("path")
+    let projection = d3.geoMercator().fitSize([width, height], geojson);
+    let path = d3.geoPath().projection(projection);
+
+
+    svg.selectAll("path").data(geojson.features).enter().append("path")
       .attr("d", path)
       .style("fill", "#ffffe8")
       .style("stroke-width", "1.5")
