e3492127b567468e21c22699f367d393c1e365d9
adwansyed <adwan.syed@ontariotechu.net>
2020-04-11 16:23:50 -0400

    Adding bicycle data layer to map

--- Bicycle Parking Vis/scripts/parking.js
+++ Bicycle Parking Vis/scripts/parking.js
@@ -2,11 +2,9 @@
 const width = 800;
 const height = 600;
 const data = "data/city_wards_data.geojson";
+const bike_data = "data/bicycle_parking_map_data.geojson";
 
 window.onload = function () {
-
-
-
   svg = d3.select("#vis").append("svg")
     .attr("width", width)
     .attr("height", height)
@@ -23,31 +21,39 @@ window.onload = function () {
 function displayGeoMap() {
 
   d3.json(data, function (err, geojson) {
-    console.log(geojson);
+    d3.json(bike_data, function (err, geojson_bike) {
 
+      console.log(geojson);
+      console.log('bike');
+      console.log(geojson_bike);
 
-    let projection = d3.geoMercator().fitSize([width, height], geojson);
-    let path = d3.geoPath().projection(projection);
+      let projection = d3.geoMercator().fitSize([width, height], geojson);
+      let path = d3.geoPath().projection(projection);
 
+      svg.selectAll("path").data(geojson.features).enter().append("path")
+        .attr("d", path)
+        .attr("class", "svg-style")  // calls the entire css class with styles, same as .style(...)
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handleMouseOut);
 
-    svg.selectAll("path").data(geojson.features).enter().append("path")
-      .attr("d", path)
-      .attr("class", "svg-style")  // calls the entire css class with styles, same as .style(...)
-      .on("mouseover", handleMouseOver)
-      .on("mouseout", handleMouseOut);
+      let bike_projection = d3.geoMercator().fitSize([width, height], geojson_bike);
+      let bike_path = d3.geoPath().projection(bike_projection);
+    
+      svg.selectAll("path").data(geojson_bike.features).enter().append("path")
+        .attr("d", bike_path)
+    })
 
-    // this displays the ward names. Not enough space to fit the names so commenting it out
-    //instead gona use hover and tooltip
+      // this displays the ward names. Not enough space to fit the names so commenting it out
+      //instead gona use hover and tooltip
 
-    /*     svg.selectAll(".ward-label")
-          .data(geojson.features)
-          .enter().append("text")
-          .attr("class", function (d) { return "ward-label " + d.id; })
-          .attr("transform", function (d) { return "translate(" + path.centroid(d) + ")"; })
-          .attr("dy", ".35em")
-          .text(function (d) { return d.properties.AREA_NAME; }) */
+      /*     svg.selectAll(".ward-label")
+            .data(geojson.features)
+            .enter().append("text")
+            .attr("class", function (d) { return "ward-label " + d.id; })
+            .attr("transform", function (d) { return "translate(" + path.centroid(d) + ")"; })
+            .attr("dy", ".35em")
+            .text(function (d) { return d.properties.AREA_NAME; }) */
   })
-
 };
 
 function handleMouseOver(d, i) {
